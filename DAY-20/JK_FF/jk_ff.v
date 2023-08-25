d16d234946618da154853cf4be6f44d80957a9a4
Chaitanya Kunda <114354982+sikeclown@users.noreply.github.com>
2023-08-26 00:01:59 +0530

    Update jk_ff.v
    From looking at the RTL code I believe that you used vivado. I found that you used clk in your if statement's argument. And I strongly believe that it is not a good practice for a synthesizable code. And also using case statement instead of if else is appropriate for this design as it doesnt need hierarchical mux but a flat mux. Look into the code in my pull request and let me know what you think. I checked the elaborated design for the new code and it work perfect. Reach out to me if you like to have a discussion. Good luck!

--- DAY-20/JK_FF/jk_ff.v
+++ DAY-20/JK_FF/jk_ff.v
@@ -18,29 +18,38 @@
 // Additional Comments: 
 //
 //////////////////////////////////////////////////////////////////////////////////
-module jk_ff(
-    input j,k,clk,
-    output reg q,qbar
+module jk_ff (
+    input j, k, clk, rst, 
+    output reg q, qbar
     );
+
+
+parameter HOLD = 2'b00, SET = 2'b10, RESET = 2'b01, TOGGLE = 2'b11;
+reg q1, q1bar;
+
+always@(*)
+begin
+    case({j,k})
+    HOLD: q1=q;
+    RESET: q1=1'b0;
+    SET: q1=1'b1;
+    TOGGLE: q1=~q;
+    default: q1=1'bz;
+    endcase 
+    q1bar=~q1;
+end
 
 always@(posedge clk)
 begin
-q=1'b0; qbar=1'b1;
-
-if(clk==1) begin
-if(j==0 && k==0) begin
-q=q; qbar=qbar; end
-else if(j==0 && k==1) begin
-q=1'b0; qbar=1'b1; end
-else if(j==1 && k==0) begin
-q=1'b1; qbar=1'b0; end
-else if(j==1 && k==1) begin
-q=~q; qbar=~qbar; end
-end
-
-if(clk==0) begin
-q=q; qbar=qbar; end
-
+    if(rst)begin
+    q<=1'b0;
+    qbar<=1'b1;
+    end
+    
+    else begin
+    q<=q1;
+    qbar<=q1bar;
+    end
 end
 
 endmodule
